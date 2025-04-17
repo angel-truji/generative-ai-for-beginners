69204d58f5bce1a7b79ff7deae17a5aa8997c2fd
Angel Trujillo <angeltruji@hotmail.com>
2025-04-16 22:23:48 -0300

    Created app.py as sandbox Python file

---
+++ 06-text-generation-apps/python/app.py
@@ -0,0 +1,10 @@
+import os
+from openai import OpenAI
+
+API_KEY = os.getenv("OPENAI_API_KEY","")
+
+client = OpenAI(
+    api_key=API_KEY
+    )
+
+chat_completion = client.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Suggest two titles for an instructional lesson on chat applications for generative AI."}])
